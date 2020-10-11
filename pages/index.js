26a475253a8d72f24670ef45390ef32e37685c55
Randy <randymoralesg@gmail.com>
2020-10-11 01:41:31 -0600

    Fix minor erros to complete the testing
    Signed-off-by: Randy <randymoralesg@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const About = ({ title, description, ...props }) => {
   return (
     <>
-      <Layout pageTitle={`${title} | About`} description={description}>
+      <Layout pageTitle={`${title}`} description={description}>
         <h1 className="title">Welcome to this demo blog!</h1>
 
         <p className="description">
